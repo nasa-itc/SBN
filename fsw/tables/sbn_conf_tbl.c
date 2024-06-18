d0facc162457c17b3a2440c1e4fc3711030b628a
Kevin Bruce <kevin.c.bruce@nasa.gov>
2024-06-18 13:43:04 -0400

    Updates to SBN Config to match Scenic Implementation (need to update with proper ports/container IDs still)

--- fsw/tables/sbn_conf_tbl.c
+++ fsw/tables/sbn_conf_tbl.c
@@ -3,51 +3,31 @@
 
 SBN_ConfTbl_t SBN_ConfTbl = {.ProtocolModules = {
                                                 {/* [0] */
-                                                  .Name        = "UDP",
-                                                  .LibFileName = "/cf/sbn_udp.so",
-                                                  .LibSymbol   = "SBN_UDP_Ops",
-                                                  .BaseEID     = 0x0100},
-                                                {/* [1] */
                                                   .Name        = "TCP",
                                                   .LibFileName = "/cf/sbn_tcp.so",
                                                   .LibSymbol   = "SBN_TCP_Ops",
                                                   .BaseEID     = 0x0200}
                                                 },
-                             .ProtocolCnt     = 2,
-                             .FilterModules   = {{/* [0] */
-                                                .Name        = "Remap",
-                                                .LibFileName = "/cf/sbn_f_remap.so",
-                                                .LibSymbol   = "SBN_F_Remap",
-                                                .BaseEID     = 0x1000}},
-                             .FilterCnt       = 1,
-
+                             .ProtocolCnt     = 1,
                              .Peers =
                                  {
-                                     {/* [0] */
+                                     {/* [0] */ //cFS
                                       .ProcessorID  = 1,
                                       .SpacecraftID = 0x42,
                                       .NetNum       = 0,
                                       .ProtocolName = "TCP",
-                                      .Filters      = {"Remap"},
-                                      .Address      = "127.0.0.1:6379",
+                                      // .Filters      = {"Remap"},
+                                      .Address      = "127.0.0.1:2234",
                                       .TaskFlags    = SBN_TASK_POLL},
-                                     {/* [1] */
+                                     {/* [1] */ //OnAIR
                                       .ProcessorID  = 2,
                                       .SpacecraftID = 0x42,
                                       .NetNum       = 0,
-                                      .ProtocolName = "UDP",
-                                      .Filters      = {"Remap"},
+                                      .ProtocolName = "TCP",
+                                      // .Filters      = {"Remap"},
                                       .Address      = "127.0.0.1:2235",
                                       .TaskFlags    = SBN_TASK_POLL},
-                                     {/* [2] */
-                                      .ProcessorID  = 3,
-                                      .SpacecraftID = 0x42,
-                                      .NetNum       = 0,
-                                      .ProtocolName = "UDP",
-                                      .Filters      = {"Remap"},
-                                      .Address      = "127.0.0.1:2236",
-                                      .TaskFlags    = SBN_TASK_POLL},
                                  },
-                             .PeerCnt = 3};
+                             .PeerCnt = 2};
 
 CFE_TBL_FILEDEF(SBN_ConfTbl, SBN.SBN_ConfTbl, SBN Configuration Table, sbn_conf_tbl.tbl)
